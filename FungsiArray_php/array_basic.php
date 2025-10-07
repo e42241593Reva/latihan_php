c212d9222577607e6a6ab11cf4fa8a91eb68822e
e42241593Reva <e42241593@student.polije.ac.id>
2025-10-07 13:52:08 +0700

    belajar fungsi array

---
+++ FungsiArray_php/array_basic.php
@@ -0,0 +1,12 @@
+<?php
+$punakawan = array("Jerapah","Gajah","Kelinci","Kucing");
+echo $punakawan[0]; //Hasilnya Jerapah
+echo "<br>";
+echo $punakawan[3]; // Hasilnya Kucing
+echo "<br>";
+$punakawan[1]="Jerapah";
+$punakawan[2]="Gajah";
+$punakawan[3]="Kelinci";
+$punakawan[4]="Kucing";
+echo $punakawan[3]; // Hasilnya Kelinci
+?>
